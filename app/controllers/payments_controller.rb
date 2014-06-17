6df466c8acf98a7708b4a985886cb7474526dcb3
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-06-17 16:29:53 +0800

    Add instance variables to Payments#one

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,10 @@
 class PaymentsController < ApplicationController
+
   def one
+    @paydollar_url = Figaro.env.PAYDOLLAR_URL
+    @merchant_id = Figaro.env.PAYDOLLAR_MERCHANT_ID
+    @amount = 3000
+    @order_id = 1
   end
 
   def success
