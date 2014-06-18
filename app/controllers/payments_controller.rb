97d69459835345c719f16969e0a0cf5e6e046e36
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-06-18 18:37:43 +0800

    Update currcode to be dynamic

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,7 @@ class PaymentsController < ApplicationController
   def one
     @paydollar_url = Figaro.env.PAYDOLLAR_URL
     @merchant_id = Figaro.env.PAYDOLLAR_MERCHANT_ID
+    @curr_code = Figaro.env.PAYDOLLAR_CURRCODE
     @amount = 3000
     @order_id = 1
   end
